dc34330376425fbf84e368206cc95351b9e1f1d0
Ryan Bachman <jacksprat455@gmail.com>
2018-07-24 13:27:15 -0500

    Array_Practice

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,83 @@
+//import java.util.ArrayList;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        ArrayList<String> squad = new ArrayList<String>();
+//        squad.add("bob");
+//        squad.add("fred");
+//        squad.add("dave");
+//        squad.add("sam");
+//        System.out.println(squad);
+//    }
+//}
+
+
+//import java.util.ArrayList;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        ArrayList<Integer> numberList = new ArrayList<Integer>();
+//        numberList.add(5);
+//        numberList.add(3);
+//        numberList.add(7);
+//        numberList.add(9);
+//        numberList.add(11);
+//        System.out.println(numberList);
+//        }
+//    }
+
+
+//EXERCISE 24
+
+//import java.util.ArrayList;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        ArrayList<Integer> numberList = new ArrayList<Integer>();
+//        numberList.add(5);
+//        numberList.add(3);
+//        numberList.add(7);
+//        numberList.add(9);
+//        numberList.add(11);
+//        System.out.println(numberList.get(1));
+//    }
+//}
+
+//
+//import java.util.ArrayList;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        ArrayList<Integer> numberList = new ArrayList<Integer>();
+//        numberList.add(5);
+//        numberList.add(3);
+//        numberList.add(7);
+//        numberList.add(9);
+//        numberList.add(11);
+//        numberList.remove(2);
+//        System.out.println(numberList);
+//    }
+//}
+
+
+
+
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> squad = new ArrayList<String>();
-        
+        ArrayList<Integer> numberList = new ArrayList<Integer>();
+        numberList.add(5);
+        numberList.add(3);
+        numberList.add(7);
+        numberList.add(9);
+        numberList.add(11);
+
+        System.out.println(numberList.get(1));
     }
 }
